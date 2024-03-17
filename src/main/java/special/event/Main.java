ef041cabcfccc4e555ae116d676365a070274bc3
ShifaaBarham <159941519+ShifaaBarham@users.noreply.github.com>
2024-03-17 13:18:22 +0200

    Add files via upload
    main changes

--- src/main/java/special/event/Main.java
+++ src/main/java/special/event/Main.java
@@ -3,15 +3,20 @@ package special.event;
 import repositories.UserRepository;
 import special.event.User;
 import components.UserComponent;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         String username;
         String password;
-        String password1;
+        String signuppass;
         String email;
+        User user1;
         String userType = "";
         String confirmPassword;
         int y = 0;
@@ -19,7 +24,6 @@ public class Main {
         String type = "";
         UserComponent userComponent = new UserComponent();
         User loggedInUser = null;
-
         System.out.println("\n\n********   Welcome to the Event Planner System   ********\n");
 
         do {
@@ -28,11 +32,24 @@ public class Main {
 
             if (x == 1) {
                 System.out.println("\n***         Login         ***\n");
-                System.out.print("Username:");
-                username = scanner.next();
-                System.out.print("Password:");
-                password = scanner.next();
-
+                while (true) {
+                    System.out.print("Username:");
+                    username = scanner.next();
+                    if (userComponent.existEmail(username)) {
+                        break;
+                    } else {
+                        System.out.print("The email you entered does not exist. Please try again\n");
+                    }
+                }
+                while (true) {
+                    System.out.print("Password:");
+                    password = scanner.next();
+                    if (userComponent.existPassword(username,password)) {
+                        break;
+                    } else {
+                        System.out.print("The Password is invalid. Please try again\n");
+                    }
+                }
                 // Validate login using UserRepository
                 loggedInUser = userComponent.validateLogin(username, password);
 
@@ -50,34 +67,55 @@ public class Main {
                 String firstName = scanner.next();
                 System.out.print("Enter last name: ");
                 String lastName = scanner.next();
+
+                boolean existEmail = false;
+
                 while (true) {
                     System.out.print("Enter email: ");
                     email = scanner.next().trim();
-                    if (userComponent.isValidEmail(email)) {
-                        break;
-                    } else {
+
+                    if (!userComponent.isValidEmail(email)) {
                         System.out.println("The email you entered is invalid. Please try again.");
+                        continue;
+                    }
+
+                    for (User user : UserRepository.users) {
+                        if (user.getEmail().equals(email)) {
+                            existEmail = true;
+                            break;
+                        }
+                    }
+
+                    if (existEmail) {
+                        System.out.println("The email you entered is already exist. Please enter another one.");
+                        existEmail = false;
+                    } else {
+                        break;
                     }
                 }
+
+
                 while (true) {
-                    System.out.print("Enter password:");
-                    password1 = scanner.next();
-                    if (userComponent.isValidPassword(password1)) {
+                    System.out.print("Enter password: ");
+                    signuppass = scanner.next().trim();
+                    if (userComponent.isValidPassword(signuppass)) {
                         break;
                     } else {
                         System.out.println("The password should contain at least 8 characters including at least one uppercase letter, one lowercase letter, one digit, and one of the following symbols: !@#$%^");
                     }
                 }
+
                 while (true) {
                     System.out.print("Confirm password:");
 
                     confirmPassword = scanner.next();
-                    if (confirmPassword.equals(password1)) {
+                    if (confirmPassword.equals(signuppass)) {
                         break;
                     } else {
                         System.out.println("The confirmed password does not match the original password.");
                     }
                 }
+
 
                 System.out.println("Enter 1 for SERVICE_PROVIDER \nOr 2 for USER");
                 do {
@@ -90,12 +128,12 @@ public class Main {
                     }
                 } while (true);
 
-                boolean isValid = userComponent.validateSignup(firstName, lastName, email, password1, confirmPassword, userType);
+                boolean isValid = userComponent.validateSignup(firstName, lastName, email, signuppass, confirmPassword, userType);
 
                 if (isValid) {
                     System.out.println("Creating an account successfully");
-                    UserRepository.addToUsers(new User(email, password1, userType, firstName, lastName));
-                    loggedInUser=userComponent.validateLogin(email,password1);
+                    UserRepository.addToUsers(new User(email, signuppass, userType, firstName, lastName));
+                    loggedInUser=userComponent.validateLogin(email,signuppass);
                     if (type.equals("SERVICE_PROVIDER")) {
                         // Store the application for service provider review
                         // Add more logic here for the service provider application
@@ -125,6 +163,16 @@ public class Main {
                 System.out.println("Logged in as an Admin.");
             else {
                 System.out.println("Logged in as a regular user.");
+                for (int i = 0; i < UserRepository.users.size(); i++) {
+                    User user = UserRepository.getfromuser(i);
+                    System.out.println("User " + (i + 1) + ":");
+                    System.out.println("Email: " + user.getEmail());
+                    System.out.println("Password: " + user.getPassword());
+                    System.out.println("Role: " + user.getType());
+                    System.out.println("First Name: " + user.getFirstName());
+                    System.out.println("Last Name: " + user.getLastName());
+                    System.out.println();
+                }
             }
         }
     }
