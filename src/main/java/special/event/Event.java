7305b902141872230c99d3ef4465839a8e7f11d3
ShifaaBarham <159941519+ShifaaBarham@users.noreply.github.com>
2024-03-24 17:54:36 +0200

    Update Event.java

--- src/main/java/special/event/Event.java
+++ src/main/java/special/event/Event.java
@@ -1,28 +1,17 @@
 package special.event;
 
-import com.thoughtworks.qdox.model.expression.Constant;
-
-import javax.xml.crypto.Data;
-import java.math.BigDecimal;
-import java.sql.Time;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Logger;
-
-import org.apache.maven.surefire.shared.lang3.ObjectUtils;
 import repositories.EventRepository;
-import repositories.UserRepository;
-
 
 public class Event {
     private String nameOfEvent;
-   private String idOfEvent;
-    public static final String[] serviceOfEvent = {"DJ", "Restaurant", "Studio", "People to organize event", "Decorations"};
-
-   private float costOfEvent;
+    private String idOfEvent;
+    public static final String[] serviceOfEvent = new String[]{"DJ", "Restaurant", "Studio", "People to organize event", "Decorations"};
+    private float costOfEvent;
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
     private static String statusOfEvent;
     private LocalDateTime eventStartTime;
@@ -30,33 +19,28 @@ public class Event {
     private Place placeOfEvent;
     User eventOwner;
 
-
-    //   public ArrayList<Reservation> timeSlots;
     public Event() {
-
     }
+
     public Event(String nameOfEvent, String idOfEvent, float costOfEvent,
-                 LocalDateTime eventStartTime, LocalDateTime eventEndTime,
-            String statusOfEvent , String nameOfPlace, int capacityOfPlace,
+                 LocalDateTime eventStartTime, LocalDateTime eventEndTime
+            , String nameOfPlace, int capacityOfPlace,
                  String locationOfPlace, String ownerEmail, String ownerPassword) {
         this.nameOfEvent = nameOfEvent;
         this.idOfEvent = idOfEvent;
         this.costOfEvent = costOfEvent;
         this.eventStartTime = eventStartTime;
         this.eventEndTime = eventEndTime;
-        this.statusOfEvent=statusOfEvent;
         placeOfEvent = new Place(nameOfPlace, capacityOfPlace, locationOfPlace);
         eventOwner = new User(ownerEmail, ownerPassword);
 
-    }
-  /*  public Event(){
-        timeSlots = new ArrayList<Reservation>();
+
     }
 
-    public Event(String name ,String id ,)*/
+
 
     public String getIdOfEvent() {
-        return idOfEvent;
+        return this.idOfEvent;
     }
 
     public void setIdOfEvent(String idOfEvent) {
@@ -64,7 +48,7 @@ public class Event {
     }
 
     public String getNameOfEvent() {
-        return nameOfEvent;
+        return this.nameOfEvent;
     }
 
     public void setNameOfEvent(String nameOfEvent) {
@@ -72,7 +56,7 @@ public class Event {
     }
 
     public float getCostOfEvent() {
-        return costOfEvent;
+        return this.costOfEvent;
     }
 
     public void setCostOfEvent(float costOfEvent) {
@@ -80,7 +64,7 @@ public class Event {
     }
 
     public LocalDateTime getEventStartTime() {
-        return eventStartTime;
+        return this.eventStartTime;
     }
 
     public void setEventStartTime(LocalDateTime eventStartTime) {
@@ -88,49 +72,58 @@ public class Event {
     }
 
     public LocalDateTime getEventEndTime() {
-        return eventEndTime;
+        return this.eventEndTime;
     }
 
     public void setEventEndTime(LocalDateTime eventEndTime) {
         this.eventEndTime = eventEndTime;
     }
-    public String getEventLocation() {  return placeOfEvent.getLocationOfPlace(); }
+
+    public String getEventLocation() {
+        return this.placeOfEvent.getLocationOfPlace();
+    }
 
     public String getstatusOfEvent() {
         return statusOfEvent;
     }
 
-    public void setstatusOfEvent( String statusOfEvent) {
-        this.statusOfEvent= statusOfEvent;
+    public void setstatusOfEvent(String statusOfEvent) {
+        Event.statusOfEvent = statusOfEvent;
     }
-
 
     public static Event theEventExists(String nameOfEvent, String idOfEvent) {
-        EventRepository eventRepository = new EventRepository();
-        for (Event event : EventRepository.events) {
-            if (event.getNameOfEvent().equals(nameOfEvent) && event.getIdOfEvent().equals(idOfEvent)) {
-                return event;
+        new EventRepository();
+        Iterator var3 = EventRepository.events.iterator();
+
+        Event event;
+        do {
+            if (!var3.hasNext()) {
+                return null;
             }
-        }
-        return null;
 
+            event = (Event)var3.next();
+        } while(!event.getNameOfEvent().equals(nameOfEvent) || !event.getIdOfEvent().equals(idOfEvent));
+
+        return event;
     }
-
 
     public static boolean checkIdOfEvent(String id) {
-        if (id.equals(0) || id.length() != 6) {
+        if (!(id.equals(0) && id.length() == 6)) {
+            return true;
+        } else {
             System.out.println("ID of event must not be  zeros or more/less than 6 numbers");
             return false;
-        } else
-            return true;
+        }
     }
+  
 
     public static boolean checkCostOfEvent(float cost) {
-        if (cost == 0 || cost < 0) {
+        if (cost != 0.0F && !(cost < 0.0F)) {
+            return true;
+        } else {
             System.out.println("Cost of event must not be  zeros negative value ");
             return false;
-        } else
-            return true;
+        }
     }
 
     public static boolean addEvent(String nameOfEvent, String idOfEvent, float costOfEvent,
@@ -140,7 +133,7 @@ public class Event {
 
         if (theEventExists(nameOfEvent, idOfEvent) == null && checkIdOfEvent(idOfEvent)== true &&checkCostOfEvent(costOfEvent)==true) {
             EventRepository R = new EventRepository();
-            R.events.add(new Event(nameOfEvent, idOfEvent, costOfEvent, eventStartTime, eventEndTime, statusOfEvent, nameOfPlace, capacityOfPlace, locationOfPlace, ownerEmail, ownerPassword));
+            R.events.add(new Event(nameOfEvent, idOfEvent, costOfEvent, eventStartTime, eventEndTime, nameOfPlace, capacityOfPlace, locationOfPlace, ownerEmail, ownerPassword));
             System.out.println("The event was added successfully");
             return true;
         } else
@@ -156,13 +149,12 @@ public class Event {
             System.out.println("The event you want to delete does not exist");
             return false;
         } else {
-            EventRepository eventRepository = new EventRepository();
+            new EventRepository();
 
-            for (int i = 0; i < EventRepository.events.size(); i++) {
-                if( EventRepository.events.get(i).getNameOfEvent().equals(name)&&EventRepository.events.get(i).getIdOfEvent().equals(id))
-                {
+            for(int i = 0; i < EventRepository.events.size(); ++i) {
+                if (((Event)EventRepository.events.get(i)).getNameOfEvent().equals(name) && ((Event)EventRepository.events.get(i)).getIdOfEvent().equals(id)) {
                     EventRepository.events.remove(i);
-                    System.out.println("The event was successfully deleted\n");
+                    System.out.println("The event was successfully deleted");
                 }
             }
 
@@ -170,48 +162,53 @@ public class Event {
         }
     }
 
-
     public Place getPlaceOfEvent() {
-        return placeOfEvent;
-    }
-    public User getEventOwner(){return eventOwner;}
-
-
-
-      public  static LocalDateTime dateConverter(String date){
-          try {
-              // Define a DateTimeFormatter for parsing the date-time string
-              DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-
-              // Parse the string to LocalDateTime using the defined formatter
-              return  LocalDateTime.parse(date, formatter);
-
-          } catch (Exception e) {
-              System.out.println("Error: Invalid input string or format");
-          }
-          return null;
-
-      }
-
-      public static void printEvent (String nameOfEvent,String idOfEvent){
-
-          for (int i = 0; i < EventRepository.events.size(); i++) {
-              if( EventRepository.events.get(i).getNameOfEvent().equals(nameOfEvent)&&EventRepository.events.get(i).getIdOfEvent().equals(idOfEvent))
-              {
-                  System.out.println("Event"+"("+i+")");
-                  System.out.println("Name Of Event :"+EventRepository.events.get(i).getNameOfEvent());
-                  System.out.println("ID Of Event :"+EventRepository.events.get(i).getIdOfEvent());
-                  System.out.println("Cost Of Event :"+ EventRepository.events.get(i).getCostOfEvent());
-                  System.out.println("Event Start Time :"+EventRepository.events.get(i).getEventStartTime());
-                  System.out.println("Event Start Time :"+EventRepository.events.get(i).getEventEndTime());
-                  System.out.println("Location Of Event :"+ EventRepository.events.get(i).getPlaceOfEvent().getLocationOfPlace());
-                  System.out.println("Place Of Event :" +EventRepository.events.get(i).getPlaceOfEvent().getNameOfPlace());
-                  System.out.println("Capacity Of Event :" +EventRepository.events.get(i).getPlaceOfEvent().getCapacityOfPlace());
-                  System.out.println("\n");
-              }
-      }
+        return this.placeOfEvent;
     }
 
-}
+    public User getEventOwner() {
+        return this.eventOwner;
+    }
 
+    public static List<Event> ReturnsEventBasedOnItsState(String statusOfEvent) {
+        List<Event> EventBasedOnItsState = new ArrayList();
+        Iterator var2 = EventRepository.events.iterator();
 
+        while(var2.hasNext()) {
+            Event event = (Event)var2.next();
+            if (event.getstatusOfEvent().equals(statusOfEvent)) {
+                EventBasedOnItsState.add(event);
+            }
+        }
+
+        return EventBasedOnItsState;
+    }
+
+    public static LocalDateTime dateConverter(String date) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+            return LocalDateTime.parse(date, formatter);
+        } catch (Exception var2) {
+            System.out.println("Error: Invalid input string or format");
+            return null;
+        }
+    }
+
+    public static void printEvent(String nameOfEvent, String idOfEvent) {
+        for(int i = 0; i < EventRepository.events.size(); ++i) {
+            if (((Event)EventRepository.events.get(i)).getNameOfEvent().equals(nameOfEvent) && ((Event)EventRepository.events.get(i)).getIdOfEvent().equals(idOfEvent)) {
+                System.out.println("Event(" + i + ")");
+                System.out.println("Name Of Event :" + ((Event)EventRepository.events.get(i)).getNameOfEvent());
+                System.out.println("ID Of Event :" + ((Event)EventRepository.events.get(i)).getIdOfEvent());
+                System.out.println("Cost Of Event :" + ((Event)EventRepository.events.get(i)).getCostOfEvent());
+                System.out.println("Event Start Time :" + String.valueOf(((Event)EventRepository.events.get(i)).getEventStartTime()));
+                System.out.println("Event Start Time :" + String.valueOf(((Event)EventRepository.events.get(i)).getEventEndTime()));
+                System.out.println("Location Of Event :" + ((Event)EventRepository.events.get(i)).getPlaceOfEvent().getLocationOfPlace());
+                System.out.println("Place Of Event :" + ((Event)EventRepository.events.get(i)).getPlaceOfEvent().getNameOfPlace());
+                System.out.println("Capacity Of Event :" + ((Event)EventRepository.events.get(i)).getPlaceOfEvent().getCapacityOfPlace());
+                System.out.println("\n");
+            }
+        }
+
+    }
+                           }
