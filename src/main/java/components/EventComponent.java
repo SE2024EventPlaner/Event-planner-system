2e38061f0400764d2017928576c5571e2ae75f78
ShifaaBarham <159941519+ShifaaBarham@users.noreply.github.com>
2024-03-25 23:29:53 +0200

    Add files via upload

--- src/main/java/components/EventComponent.java
+++ src/main/java/components/EventComponent.java
@@ -9,8 +9,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class EventComponent {
-public static Event theEventExists(String nameOfEvent, String idOfEvent) {
-    EventRepository eventRepository = new EventRepository();
+    public EventRepository eventRepository = new EventRepository();
+public  Event theEventExists(String nameOfEvent, String idOfEvent) {
+
     for (Event event : EventRepository.events) {
         if (event.getNameOfEvent().equals(nameOfEvent) && event.getIdOfEvent().equals(idOfEvent)) {
             return event;
@@ -19,7 +20,7 @@ public static Event theEventExists(String nameOfEvent, String idOfEvent) {
     return null;
 
 }
-public static boolean checkSimilarityEvent(String nameOfPlace, LocalDateTime eventStartTime, LocalDateTime eventEndTime, String locationOfPlace) {
+public  boolean checkSimilarityEvent(String nameOfPlace, LocalDateTime eventStartTime, LocalDateTime eventEndTime, String locationOfPlace) {
     for (Event event : EventRepository.events) {
         if (event.getPlaceOfEvent().getNameOfPlace().equals(nameOfPlace) &&
                 event.getEventStartTime().isEqual(eventStartTime) &&
@@ -32,7 +33,7 @@ public static boolean checkSimilarityEvent(String nameOfPlace, LocalDateTime eve
 }
 
 
-public static boolean checkIdOfEvent(String id) {
+public  boolean checkIdOfEvent(String id) {
     if (id.equals(0) || id.length() != 6) {
         System.out.println("ID of event must not be  zeros or more/less than 6 numbers");
         return false;
@@ -40,7 +41,7 @@ public static boolean checkIdOfEvent(String id) {
         return true;
 }
 
-public static boolean checkCostOfEvent(float cost) {
+public  boolean checkCostOfEvent(float cost) {
     if (cost == 0 || cost < 0) {
         System.out.println("Cost of event must not be  zeros negative value ");
         return false;
@@ -48,7 +49,7 @@ public static boolean checkCostOfEvent(float cost) {
         return true;
 }
 
-    public  static LocalDateTime dateConverter(String date){
+    public   LocalDateTime dateConverter(String date){
         try {
             // Define a DateTimeFormatter for parsing the date-time string
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
@@ -63,12 +64,11 @@ public static boolean checkCostOfEvent(float cost) {
 
     }
 
-    public static boolean deleteEvent(String name, String id) {
+    public  boolean deleteEvent(String name, String id) {
         if (theEventExists(name, id) == null) {
             System.out.println("The event you want to delete does not exist");
             return false;
         } else {
-            EventRepository eventRepository = new EventRepository();
 
             for (int i = 0; i < EventRepository.events.size(); i++) {
                 if( EventRepository.events.get(i).getNameOfEvent().equals(name)&&EventRepository.events.get(i).getIdOfEvent().equals(id))
@@ -81,13 +81,12 @@ public static boolean checkCostOfEvent(float cost) {
             return true;
         }
     }
-    public static boolean addEvent(String nameOfEvent, String idOfEvent, float costOfEvent,
+    public  boolean addEvent(String nameOfEvent, String idOfEvent, float costOfEvent,
                                    LocalDateTime eventStartTime, LocalDateTime eventEndTime
                                    , String nameOfPlace, int capacityOfPlace,
                                    String locationOfPlace, String ownerEmail, String ownerPassword) {
 
         if (theEventExists(nameOfEvent, idOfEvent) == null && checkIdOfEvent(idOfEvent) &&checkCostOfEvent(costOfEvent)) {
-            EventRepository R = new EventRepository();
             EventRepository.events.add(new Event(nameOfEvent, idOfEvent, costOfEvent, eventStartTime, eventEndTime, nameOfPlace, capacityOfPlace, locationOfPlace, ownerEmail, ownerPassword));
             System.out.println("The event was added successfully");
             return true;
@@ -98,7 +97,7 @@ public static boolean checkCostOfEvent(float cost) {
 
     }
 
-    public static void printEvent (String nameOfEvent,String idOfEvent){
+    public  void printEvent (String nameOfEvent,String idOfEvent){
 
         for (int i = 0; i < EventRepository.events.size(); i++) {
             if( EventRepository.events.get(i).getNameOfEvent().equals(nameOfEvent)&&EventRepository.events.get(i).getIdOfEvent().equals(idOfEvent))
