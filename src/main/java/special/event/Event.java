13840e5ca3ef0ae3fb142b677651ca94dd6a9184
shahdHennawi <159945061+shahdHennawi@users.noreply.github.com>
2024-03-30 14:47:10 +0200

    Add files via upload
    update

--- src/main/java/special/event/Event.java
+++ src/main/java/special/event/Event.java
@@ -1,75 +1,59 @@
 package special.event;
 
-import com.thoughtworks.qdox.model.expression.Constant;
-
-import javax.xml.crypto.Data;
-import java.math.BigDecimal;
 import java.nio.file.Path;
-import java.sql.Time;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.logging.Logger;
 
-import org.apache.maven.surefire.shared.lang3.ObjectUtils;
-import repositories.EventRepository;
 import repositories.UserRepository;
 
 
 public class Event {
     private String nameOfEvent;
-   private String idOfEvent;
+    private String idOfEvent;
     public static final String[] serviceOfEvent = {"DJ", "Restaurant", "Studio", "People to organize event", "Decorations"};
-
-   private float costOfEvent;
-   private float constructionCostOfEvent;
+    private float costOfEvent;
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-    private static String statusOfEvent;
-    private LocalDateTime eventStartTime;
-    private LocalDateTime eventEndTime;
-    private Place placeOfEvent;
+    String statusOfEvent="unbook";
+    LocalDateTime eventStartTime;
+    LocalDateTime eventEndTime;
+    Place placeOfEvent;
     User eventOwner;
-    private User bookedUser;
     Path imagePath;
-    float eventConstructionCost;
-
-
-
-    //   public ArrayList<Reservation> timeSlots;
+    private User bookedUser;
     public Event() {
 
     }
-    public Event(String nameOfEvent, String idOfEvent, float costOfEvent,float  constructionCostOfEvent,
+    public Event(String nameOfEvent, String idOfEvent, float costOfEvent,
                  LocalDateTime eventStartTime, LocalDateTime eventEndTime,
                  String nameOfPlace, int capacityOfPlace,
                  String locationOfPlace, String ownerEmail, String ownerPassword) {
         this.nameOfEvent = nameOfEvent;
         this.idOfEvent = idOfEvent;
         this.costOfEvent = costOfEvent;
-        this.constructionCostOfEvent=constructionCostOfEvent;
         this.eventStartTime = eventStartTime;
         this.eventEndTime = eventEndTime;
         placeOfEvent = new Place(nameOfPlace, capacityOfPlace, locationOfPlace);
         for (User user: UserRepository.users){
-            if(user.getEmail().equals(ownerEmail) && user.getPassword().equals(ownerPassword))
+            if(user.getEmail().equals(ownerEmail) && user.getPassword().equals(ownerPassword)){
                 eventOwner =user;
-
-    }
+            }
         }
-
-
+    }
     public String getIdOfEvent() {
         return idOfEvent;
+    }
+    public void setpath(Path p) {
+        this.imagePath = p;
+    }
+
+    public Path getpath() {
+        return imagePath;
     }
 
     public void setIdOfEvent(String idOfEvent) {
         this.idOfEvent = idOfEvent;
     }
 
-     
-    
     public String getNameOfEvent() {
         return nameOfEvent;
     }
@@ -85,16 +69,6 @@ public class Event {
     public void setCostOfEvent(float costOfEvent) {
         this.costOfEvent = costOfEvent;
     }
-
-    public float getConstructionCostOfEvent() {
-        return constructionCostOfEvent;
-    }
-
-    public void setConstructionCostOfEvent(float constructionCostOfEvent) {
-        this.costOfEvent = constructionCostOfEvent;
-    }
-
-
 
     public LocalDateTime getEventStartTime() {
         return eventStartTime;
@@ -121,15 +95,17 @@ public class Event {
         this.statusOfEvent= statusOfEvent;
     }
 
-
-
-    public void setpath(Path p) {
-        this.imagePath = p;
+    public User getBookedUser() {
+        return bookedUser;
+    }
+    public void setBookedUser(User bookedUser) {
+        this.bookedUser = bookedUser;
+    }
+    public void setEventStatus(String status) {
+        this.statusOfEvent = status;
     }
 
-    public Path getpath() {
-        return imagePath;
-    }
+
 
 
     public Place getPlaceOfEvent() {
@@ -138,12 +114,13 @@ public class Event {
     public User getEventOwner(){return eventOwner;}
 
 
-
-  
-
-
-
-
+    public boolean bookEvent(User user) {
+        if (this.bookedUser == null) {
+            this.bookedUser = user;
+            return true;
+        } else {
+            System.out.println("Event is already booked.");
+            return false;
+        }
+    }
 }
-
-
