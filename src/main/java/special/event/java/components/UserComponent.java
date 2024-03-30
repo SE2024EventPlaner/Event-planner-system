00b8ea668b6b7e5023fa1189eaf233834ab834f5
aseelzaid <159944921+aseelzaid@users.noreply.github.com>
2024-03-30 05:09:38 +0200

    Add files via upload

--- src/main/java/special/event/java/components/UserComponent.java
+++ src/main/java/special/event/java/components/UserComponent.java
@@ -1,18 +1,48 @@
 package components;
 
-import org.apache.maven.surefire.shared.lang3.ObjectUtils;
 import repositories.UserRepository;
 import special.event.User;
 import java.util.regex.*;
 
-
 public class UserComponent {
     private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
     private static final Pattern pattern = Pattern.compile(EMAIL_REGEX);
+    public UserRepository repo=new UserRepository();
+    public User validateLogin(String username, String password){
 
-    public User validateLogin(String username, String password) {
+        for (User user: UserRepository.users){
+            if(user.getEmail().equals(username) && user.getPassword().equals(password)){
+                return user;
+            }
+        }
+        return null;
+    }
+    public boolean existEmail(String email) {
         for (User user : UserRepository.users) {
-            if (user.getEmail().equals(username) && user.getPassword().equals(password)) {
+            if (user.getEmail().equals(email) ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean existPassword(String username,String password){
+        for (User user: UserRepository.users){
+            if(user.getEmail().equals(username) ){
+                if (user.getPassword().equals(password))
+                    return true;
+                else
+                    return false;
+            }
+        }
+        return false;
+    }
+
+
+    public User validateLoginAsServiceProvider(String username, String password){
+        UserRepository userRepository = new UserRepository();
+        for (User user: UserRepository.users){
+            if(user.getEmail().equals(username) && user.getPassword().equals(password)&&user.getType().equals("SERVICE_PROVIDER")){
                 return user;
             }
         }
@@ -20,8 +50,9 @@ public class UserComponent {
     }
 
 
-    public boolean validateUserType(User user, String type) {
 
+
+    public boolean validateUserType(User user, String type){
         return user != null && user.getType().equals(type);
     }
 
@@ -38,13 +69,14 @@ public class UserComponent {
     }
 
     public boolean isValidPassword(String password) {
-        if (password.length() < 8 || password.length() > 20) {
+        if (password.length() < 8) {
             return false;
         }
 
-        String pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$";
+        String pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\p{Punct}])[A-Za-z\\d\\p{Punct}]+$";
         return password.matches(pattern);
     }
 
 
 }
+
