8f4c08cc16e77b7934d4569fac442608bbaa72a8
Shifaa Barham <159941519+ShifaaBarham@users.noreply.github.com>
2024-04-01 12:49:12 +0200

    Update BookingSystem.java

--- src/main/java/special/event/BookingSystem.java
+++ src/main/java/special/event/BookingSystem.java
@@ -1,39 +1,38 @@
 package special.event;
 import java.time.LocalDateTime;
-import java.util.Scanner;
 import repositories.EventRepository;
+import java.util.logging.Logger;
 public class BookingSystem {
+    private static final Logger logger3 = Logger.getLogger(BookingSystem.class.getName());
     public static boolean bookEvent(String eventId, String location, LocalDateTime bookingDate, float userBalance,User loggedInUser) {
         Event event = findEventByIdAndLocation(eventId,location);
-        if (loggedInUser.bookedEvent2.size()==0){
+        if (loggedInUser.bookedEvent2.isEmpty()){
             loggedInUser.setAccountBalance(userBalance);}
         else {
             loggedInUser.setAccountBalance(loggedInUser.getAccountBalance());}
         if (event != null) {
             if (isBookingDateValid(bookingDate,event)){
                 if (loggedInUser.getAccountBalance() >= event.getCostOfEvent() ) {
-                    // add to arraylist event
                     loggedInUser.bookedEvent1.add(event);
                     Notification ReservationNotification = new Notification();
                     ReservationNotification.createReservationRequest(loggedInUser,event);
                     ReservationNotification.sendReservationRequest();
-                    System.out.println("We will send you a notification when your reservation request is accepted !");
-
+                    logger3.info("We will send you a notification when your reservation request is accepted !");
                     return true;
                 } else  {
-                    System.out.println("Insufficient funds. Booking failed.");
+                    logger3.info("Insufficient funds. Booking failed.");
                 }
             } else {
-                System.out.println("Invalid booking date. Please choose a future date.");
+                logger3.info("Invalid booking date. Please choose a future date.");
             }
         } else {
-            System.out.println("Event with ID " + eventId + " in " + location + " not available for booking.");
+            logger3.info("Event with ID " + eventId + " in " + location + " not available for booking.");
         }
         return false;
     }
     public static Event findEventByIdAndLocation(String eventId, String location) {
         EventRepository eventRepository = new EventRepository();
-        for (Event event : eventRepository.events) {
+        for (Event event : EventRepository.events) {
             if (event.getIdOfEvent().equals(eventId) && event.getPlaceOfEvent().getLocationOfPlace().equals(location)) {
                 return event;
             }
@@ -41,32 +40,25 @@ public class BookingSystem {
         return null;
     }
     public static boolean processPayment(String CardNumber,Event event,User loggedInUser) {
-        // Validate card number format
         if (!isValidCardNumberFormat(CardNumber)) {
-            System.out.println("Invalid card number format.");
+            logger3.info("Invalid card number format.");
             return false;
         } else {
             float value = loggedInUser.getAccountBalance()- event.getCostOfEvent();
             loggedInUser.setAccountBalance(value) ;
             loggedInUser.bookedEvent2.add(event);
-            System.out.println("Booking successful! ");
+            logger3.info("Booking successful! ");
         }
-
         return  true;
     }
-    public static boolean isValidCardNumberFormat(String CardNumber) {
-        // Check if the card number is a 16-digit numeric string
-        return CardNumber.matches("^\\d{16}$");
+    public static boolean isValidCardNumberFormat(String cardNumber) {
+        return cardNumber.matches("^\\d{16}$");
     }
     public static boolean isBookingDateValid(LocalDateTime bookingDate,Event event) {
         return event.getEventStartTime().equals(bookingDate);
     }
-    public static boolean isUserBalanceSufficient (float userBalance,Event event) {
-        if(event.getCostOfEvent()<= userBalance)
-            return true;
-        else
-            return false;
-
+    public static boolean isUserBalanceSufficient(float userBalance, Event event) {
+        return event.getCostOfEvent() <= userBalance;
     }
 
 }
