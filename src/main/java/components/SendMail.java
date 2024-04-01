e38566852b3038585d0b46f5b1ec9cbac432c0ed
Shifaa Barham <159941519+ShifaaBarham@users.noreply.github.com>
2024-04-01 12:25:06 +0200

    Update SendMail.java

--- src/main/java/components/SendMail.java
+++ src/main/java/components/SendMail.java
@@ -4,47 +4,41 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class SendMail {
-    public static void getSendEmail(String toUserMessage, String email) {
-        // Recipient's email ID needs to be mentioned.
-        String to = email;
+    private static final Logger logger = Logger.getLogger(SendMail.class.getName());
 
+    private SendMail() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static void sendEmail(String toUserMessage, String email) {
         String from = "aaadmain78@gmail.com";
-
         String host = "smtp.gmail.com";
 
         Properties properties = System.getProperties();
-
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", "587");
         properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.auth", "true");
 
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-
             protected PasswordAuthentication getPasswordAuthentication() {
-                // Use an app password generated from your Google Account settings
                 return new PasswordAuthentication("aaadmain78@gmail.com", "vxsckjrirjzqnrvx");
             }
-
         });
-
 
         try {
             MimeMessage message = new MimeMessage(session);
-
             message.setFrom(new InternetAddress(from));
-
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(email)); 
             message.setSubject("Your Account Status!!");
-
             message.setText("Your request to create a business account on the Event Planner System : \n " + toUserMessage);
 
-            System.out.println("sending...");
+            logger.info("Sending...");
             Transport.send(message);
-            System.out.println("Sent message successfully....");
+            logger.info("Sent message successfully.");
         } catch (MessagingException mex) {
             mex.printStackTrace();
         }
